2e959a967e88bcd5311ec1bcd060815268b95306
Jonathan Lin <jonathan.lin@ninjatrader.com>
2025-03-24 19:54:06 -0400

    Added import/order and other eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,10 +26,36 @@ export default [
             '@typescript-eslint/no-explicit-any': 'warn',
             'no-console': 'warn',
             'no-unused-vars': 'off',
-            '@typescript-eslint/no-unused-vars': ['error', {
-                'argsIgnorePattern': '^_',
-                'varsIgnorePattern': '^_'
-            }],
+            '@typescript-eslint/no-unused-vars': 'warn',
+            'import/order': [
+                'warn',
+                {
+                    groups: ['builtin', 'external', 'internal'],
+                    pathGroups: [
+                        {
+                            pattern: 'react',
+                            group: 'external',
+                            position: 'before'
+                        },
+                        {
+                            pattern: '@/**',
+                            group: 'internal',
+                            position: 'after'
+                        }
+                    ],
+                    pathGroupsExcludedImportTypes: ['react'],
+                    'newlines-between': 'always',
+                    alphabetize: {
+                        order: 'asc',
+                        caseInsensitive: false
+                    }
+                }
+            ],
+            'import/no-cycle': 'warn',
+            'no-use-before-define': [
+                'error',
+                { functions: false, classes: false, variables: false }
+            ]
         },
     },
     prettierConfig,
